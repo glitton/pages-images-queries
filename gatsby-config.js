aaddd21ef4616d92807fa001ce23cdbd928ad3cb
Generosa Litton <glcodeworks@gmail.com>
2022-03-05 10:35:56 -0800

    added contentful source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,57 @@
+const path = require(`path`);
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+// If you want to use the preview API please define
+// CONTENTFUL_HOST and CONTENTFUL_PREVIEW_ACCESS_TOKEN in your
+// environment config.
+//
+// CONTENTFUL_HOST should map to `preview.contentful.com`
+// CONTENTFUL_PREVIEW_ACCESS_TOKEN should map to your
+// Content Preview API token
+//
+// For more information around the Preview API check out the documentation at
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+//
+// To change back to the normal CDA, remove the CONTENTFUL_HOST variable from your environment.
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
+  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+}
+
+console.log(JSON.stringify(contentfulConfig));
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  );
+}
+
 module.exports = {
+  // Default Page Meta Data
   siteMetadata: {
-    title: `Gatsby and Later Session`,
-    siteUrl: `https://www.later.com`,
-    authors: `The Later team`,
-    description: `Exploring the wonderful world of Gatsby`,
+    title: "World’s Favorite Instagram Marketing Platform | Later",
+    description:
+      "The leading visual marketing platform that allows you to plan, schedule, publish, and measure the results of your instagram and social media posts.",
+    siteUrl: "https://later.com", // No trailing slash allowed!
+    image: "/images/homepage/home_meta.png", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@latermedia",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: contentfulConfig,
+    },
+  ],
 };
